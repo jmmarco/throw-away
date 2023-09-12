97ffebb5b00cda709adc3bc2c0da245e28a658d4
Juan Marco <jmmarco@gmail.com>
2023-09-12 12:07:56 +0200

    Remove semicolon and trailing comma

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import App from './App'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
-);
+)
