855158adee482839dbc28c88fe5e0586869f2fba
Juan Marco <jmmarco@gmail.com>
2023-09-12 11:30:32 +0200

    Add semicolons on purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import './App.css'
-import { useQuery } from 'react-query'
+import React from 'react';
+import './App.css';
+import { useQuery } from 'react-query';
 
 function fetchUser(username) {
-  return fetch(`https://api.github.com/users/${username}`).then((res) => res.json())
+  return fetch(`https://api.github.com/users/${username}`).then((res) => res.json());
 }
 
 function GithubUser({ username }) {
-  const userQuery = useQuery([username], () => fetchUser(username))
+  const userQuery = useQuery([username], () => fetchUser(username));
 
-  const { data, isLoading, isError } = userQuery
+  const { data, isLoading, isError } = userQuery;
 
-  if (isLoading) return <p>Loading...</p>
+  if (isLoading) return <p>Loading...</p>;
 
   if (isError) {
-    return <div>{userQuery.error.message}</div>
+    return <div>{userQuery.error.message}</div>;
   }
 
   if (!username) {
-    return <input type="text" placeholder="defunkt" />
+    return <input type="text" placeholder="defunkt" />;
   }
 
-  return <pre style={{ fontSize: '2 rem' }}>{JSON.stringify(data, null, 2)}</pre>
+  return <pre style={{ fontSize: '2 rem' }}>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 function App() {
@@ -33,7 +33,7 @@ function App() {
         <GithubUser username="" />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
