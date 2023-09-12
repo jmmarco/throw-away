2dee5f78353611be9e3c59f7ef49917d7d2322fb
Juan Marco <jmmarco@gmail.com>
2023-09-12 12:10:08 +0200

    Fix remaining code style issues

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import App from './App'
 
 // eslint-disable-next-line no-undef
 test('renders learn react link', () => {
-  render(<App />);
-  const text = screen.getByText(/Welcome to new app!/i);
+  render(<App />)
+  const text = screen.getByText(/Welcome to new app!/i)
   // eslint-disable-next-line no-undef
-  expect(text).toBeInTheDocument();
-});
+  expect(text).toBeInTheDocument()
+})
