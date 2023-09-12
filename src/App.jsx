b9d41914b375f11e498f64cdcaa7d56fe87da3a4
Juan Marco <jmmarco@gmail.com>
2023-09-12 12:36:02 +0200

    Feature/add prettier (#10)
    * Turn off semicolons for Prettier/ESLint
    
    * Add React Query
    
    * Pass multiple values to lint/fix scripts
    
    * Setup React Query
    
    * Add Prettier and corresponding scripts
    
    * Remove trailing comma option
    
    * Reconfigure/Rename scripts
    
    * Add build name for workflow
    
    * Remove semicolon and trailing comma
    
    * Fix remaining code style issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import './App.css';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import './App.css'
+import { useQuery } from 'react-query'
+
+function fetchUser(username) {
+  return fetch(`https://api.github.com/users/${username}`).then((res) => res.json())
+}
+
+function GithubUser({ username }) {
+  const userQuery = useQuery([username], () => fetchUser(username))
+
+  const { data, isLoading, isError } = userQuery
+
+  if (isLoading) return <p>Loading...</p>
+
+  if (isError) {
+    return <div>{userQuery.error.message}</div>
+  }
+
+  if (!username) {
+    return <input type="text" placeholder="defunkt" />
+  }
+
+  return <pre style={{ fontSize: '2 rem' }}>{JSON.stringify(data, null, 2)}</pre>
+}
 
 function App() {
   return (
     <div className="App">
       <main className="App-main">
-        Welcome to new app!
+        <h2>Welcome to new app!</h2>
+        <GithubUser username="" />
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
